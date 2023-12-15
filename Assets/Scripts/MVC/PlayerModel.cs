8de36ec1ed6ef93d4d02a450d87c4786ff4b9adf
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-12-15 19:28:59 -0300

    Revert "Se aplico el memento"
    This reverts commit 96eea1603f29dace650fac1316f849fc7e768893.

--- Assets/Scripts/MVC/PlayerModel.cs
+++ Assets/Scripts/MVC/PlayerModel.cs
@@ -5,7 +5,6 @@ public class PlayerModel : MonoBehaviour
     public float speed = 5f;
     public float jumpForce = 10f;
     public int maxJumps = 2;
-    private int life = 100;
 
     public Rigidbody rb;
 
@@ -22,27 +21,5 @@ public class PlayerModel : MonoBehaviour
     public void Jump()
     {
         rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
-    }
-
-    public PlayerLifeMemento CreateMemento()
-    {
-        return new PlayerLifeMemento(life);
-    }
-
-    public void RestoreFromMemento(PlayerLifeMemento memento)
-    {
-        life = memento.Life;
-    }
-
-    public void TakeDamage(int damage)
-    {
-        life -= damage;
-        Debug.Log("Player took damage. Current life: " + life);
-    }
-
-    public void RecoverLife(int recoveryAmount)
-    {
-        life += recoveryAmount;
-        Debug.Log("Player recovered life. Current life: " + life);
     }
 }
