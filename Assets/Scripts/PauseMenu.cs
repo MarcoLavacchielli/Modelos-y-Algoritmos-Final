59db490a5e23d8418c8ab9f54bfd41071be1b49a
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-12-18 18:19:01 -0300

    Pausa hecha

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseCanvas;
+    private bool isPaused = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f; // Pausar el tiempo en el juego
+            pauseCanvas.SetActive(true); // Mostrar el canvas de pausa
+        }
+        else
+        {
+            Time.timeScale = 1f; // Reanudar el tiempo en el juego
+            pauseCanvas.SetActive(false); // Ocultar el canvas de pausa
+        }
+    }
+
+    public void loadscene(string scenename) //carga una escena especifica por nombre
+    {
+        Time.timeScale = 1f; // Reanudar el tiempo en el juego
+        SceneManager.LoadScene(scenename);
+    }
+}
