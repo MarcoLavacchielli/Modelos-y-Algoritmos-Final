81d929114118aee13f69f35b3dbe43ed981f642e
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-12-19 19:41:37 -0300

    Se modifico el ataque y el respawn

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -10,32 +10,32 @@ public class PlayerAttack : MonoBehaviour
 
     [SerializeField] private GameObserver gameObserver;
 
+    [SerializeField] private ParticleSystem attackPs;
+
+    [SerializeField] private float cooldown = 0.5f;
+    private float nextAttackTime = 0f;
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && Time.time >= nextAttackTime)
         {
             Attack();
+            nextAttackTime = Time.time + cooldown;
         }
     }
 
     void Attack()
     {
-        //Debug.Log("ataca");
         Collider[] hitEnemies = Physics.OverlapSphere(transform.position, attackRadius, enemyLayer);
+        attackPs.Play();
 
         foreach (Collider enemy in hitEnemies)
         {
-            //Debug.Log("ataca enemigos");
             Enemy enemyScript = enemy.GetComponent<Enemy>();
             if (enemyScript != null)
             {
                 enemyScript.TakeDamage(danio);
-                //Debug.Log("daño hecho");
-            }
-            else
-            {
-                //Debug.Log("no hizo daño");
             }
         }
     }
@@ -48,19 +48,16 @@ public class PlayerAttack : MonoBehaviour
 
     private void OnEnable()
     {
-        // Suscribe al evento cuando el objeto está activo
         gameObserver.DamageChanged += HandleDamageChanged;
     }
 
     private void OnDisable()
     {
-        // Desuscribe al evento cuando el objeto se desactiva para evitar pérdida de referencia
         gameObserver.DamageChanged -= HandleDamageChanged;
     }
 
     private void HandleDamageChanged(int amount)
     {
-        // Actualiza el daño según el evento recibido
         danio += amount;
     }
 }
