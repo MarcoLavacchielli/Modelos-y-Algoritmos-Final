0df54e87d16289bd313269b6c67c63361db7d193
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2023-12-20 13:36:47 -0300

    Particulas en power up

--- Assets/Scripts/PowerUps/PowerUpDamage.cs
+++ Assets/Scripts/PowerUps/PowerUpDamage.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PowerUpDamage : MonoBehaviour
 {
+
+    [SerializeField] private ParticleSystem powerDamagePs;
+
     private void OnTriggerEnter(Collider other)
     {
         other.GetComponent<GameObserver>().IncreaseDamage(1);
+        powerDamagePs.Play();
         Destroy(gameObject);
     }
 }
--- Assets/Scripts/PowerUps/PowerUpVelocity.cs
+++ Assets/Scripts/PowerUps/PowerUpVelocity.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class PowerUpVelocity : MonoBehaviour
 {
+    [SerializeField] private ParticleSystem powerVelocityPs;
+
     private void OnTriggerEnter(Collider other)
     {
         other.GetComponent<GameObserver>().IncreaseSpeed(3f);
+        powerVelocityPs.Play();
         Destroy(gameObject);
     }
 }
